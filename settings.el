0200cfed00eca67483a7c61fbeb497489c4d3779
Martin Stendorf <mstendorf@gmail.com>
2015-04-11 23:22:36 +0200

    Fixed utf-8 handling it seems!

--- settings.el
+++ settings.el
@@ -9,11 +9,14 @@
 
 
 ;setup the preference of UTF-8
-(setq locale-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
+(when (display-graphic-p)
+  (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING)))
+;; (setq locale-coding-system 'utf-8)
+;; (set-terminal-coding-system 'utf-8)
+;; (set-keyboard-coding-system 'utf-8)
+;; (set-selection-coding-system 'utf-8)
+;; (prefer-coding-system 'utf-8)
 
 ; auto reload buffers that change on disk so that we never edit stale buffers
 (global-auto-revert-mode)
