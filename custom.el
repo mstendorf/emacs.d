1f6497bd3d7b6ccdfc4d60d7a6601deaf4eae853
Martin Stendorf <mstendor@gmail.com>
2015-10-14 23:21:49 +0200

    Fixed autocomplete via elpy using jedi backend.

--- custom.el
+++ custom.el
@@ -6,6 +6,7 @@
  '(custom-safe-themes
    (quote
     ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
+ '(elpy-rpc-backend "jedi")
  '(send-mail-function (quote smtpmail-send-it))
  '(smtpmail-smtp-server "84.238.0.3")
  '(smtpmail-smtp-service 25))
@@ -14,4 +15,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(company-scrollbar-bg ((t (:background "#ffffff"))))
+ '(company-scrollbar-fg ((t (:background "#ffffff"))))
+ '(company-tooltip ((t (:inherit default :background "#ffffff"))))
+ '(company-tooltip-common ((t (:inherit font-lock-constant-face))))
+ '(company-tooltip-selection ((t (:inherit font-lock-function-name-face)))))
